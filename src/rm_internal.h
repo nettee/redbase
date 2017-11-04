f50c6b176ad939ec0a23dec913cca8975550aaf1
William <nettee.liu@gmail.com>
2017-11-04 10:52:39 +0800

    put rm_filehdr back to rm.h

--- src/rm_internal.h
+++ src/rm_internal.h
@@ -1,21 +1,7 @@
-//
-// File:        pf_internal.h
-// Description: Declarations internal to the paged file component
-// Authors:     Hugo Rivero (rivero@cs.stanford.edu)
-//              Dallan Quass (quass@cs.stanford.edu)
-//              Jason McHugh (mchughj@cs.stanford.edu)
-//
-
 #ifndef RM_INTERNAL_H_
 #define RM_INTERNAL_H_
 
 #include "rm.h"
 
-/*
- * RM_FileHdr: Header structure for files
- */
-struct RM_FileHdr {
-	int recordSize; // size of each record
-};
 
 #endif /* RM_INTERNAL_H_ */
